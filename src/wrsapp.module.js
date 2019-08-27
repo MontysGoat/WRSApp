d33fb1f56b5738b2281b2aa34b1a3c7cea80bef0
George Hearn <george.hearn@schneider-electric.com>
2019-08-27 08:44:35 -0400

    mobile menu closes on link click

--- src/wrsapp.module.js
+++ src/wrsapp.module.js
@@ -11,6 +11,12 @@
   function config($urlRouterProvider, $locationProvider) {
 
     $urlRouterProvider.otherwise('/about');
+
+    $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+          $(this).collapse('hide');
+      }
+  });
   }
 
 })();
